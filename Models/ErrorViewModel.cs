1d3c8569ac6550e24fcce623a9f4a0afbcf65fb8
olu <adeoyeseunengineer@hotmail.com>
2023-11-17 12:35:02 -0600

    Commit first changes

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace scarstacksolution.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
